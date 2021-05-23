24b1a00a1612df0111210b36c06cb3541e2ad291
yash1703 <yashkoolwal2019@gmail.com>
2021-05-24 00:26:59 +0530

    feat
    connected to the mongoDB server

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const {initializeDBConnection} = require('./db/db.connect');
+
+const {errorHandler} = require('./handler/errorHandler');
+const {routeHandler} = require('./handler/routeHandler');
+
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 const app = express();
-let port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+initializeDBConnection();
+
 
 app.get("/", (req,res)=>{
     res.send("Hello World!");
 })
 
-app.get("/user", (req, res)=>{
-    res.json({message:"sever connected successfully"});
-})
 
 
-app.get("/profile", (req, res)=>{
-    res.json({message:"you are on profile page"});
-})
+//route handler
+app.use(routeHandler);
 
-app.listen(port, ()=>{
-    console.log(`Express app is listeing on port http://localhost:${port}`);
+//error handler
+app.use(errorHandler);
+
+
+app.listen(PORT, ()=>{
+    console.log(`Express app is listeing on port http://localhost:${PORT}`);
 });
