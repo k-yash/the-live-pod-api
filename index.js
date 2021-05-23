49932ee2344cc8ad171593ed6e65bfcbb40cafa8
yash1703 <yashkoolwal2019@gmail.com>
2021-05-23 15:45:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req,res)=>{
     res.send("Hello World!");
 })
 
-app.get("/users", (req, res)=>{
+app.get("/user", (req, res)=>{
     res.json({message:"sever connected successfully"});
 })
 
